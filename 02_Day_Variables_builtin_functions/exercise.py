0508f8e3834652f19942fff0e53f30f1af45fc32
OMGboom7 <novaboom1029@gmail.com>
2023-08-01 21:49:35 +0800

    exercise day 2

---
+++ 02_Day_Variables_builtin_functions/exercise.py
@@ -0,0 +1,22 @@
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
+
+import math
+
+radius = 30
+area_of_circle = math.pi * radius * radius
+circum_of_circle = 2 * math.pi * radius
+print(area_of_circle)
+print(circum_of_circle)
+
+radius2 = int(input('radius:'))
+area_of_circle2 = math.pi * radius2 * radius2
+circum_of_circle2 = 2 * math.pi * radius2
+print(area_of_circle2)
+print(circum_of_circle2)
